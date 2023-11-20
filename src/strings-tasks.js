1c3523ddeea51955de7cb53320a411c6869e34a8
Aliaksandr Fedukovich <1632637@mail.ru>
2023-11-20 15:27:51 +0300

    feat: add solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -311,8 +311,9 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const newStr = str.replaceAll(/\W/g, '').toLowerCase();
+  return newStr === newStr.split('').reverse().join('');
 }
 
 /**
